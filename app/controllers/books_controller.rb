be6bced9fc288bb2e86b960678145bc7c1133270
mamekoko <129962215+mamekoko@users.noreply.github.com>
2023-05-09 13:39:22 +0000

    bookers_app

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,17 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    if @book.save
     flash[:notice] = "Book was successfully created." 
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def index
-    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -23,10 +26,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated." 
-    redirect_to book_path(book.id)  
+    redirect_to book_path(@book.id)  
+    else
+    render :edit
+    end
   end
   
    def destroy
